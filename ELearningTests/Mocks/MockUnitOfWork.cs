5c5c3702629dd9053449cf89a593670596c358f9
Nees612 <vekker15@gmai.com>
2020-03-31 19:56:01 +0200

    Created test project

---
+++ ELearningTests/Mocks/MockUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NSubstitute;
+
+namespace ELearningTests.Mocks
+{
+    class MockUnitOfWork
+    {
+        private 
+    }
+}
---
+++ ELearningTests/UsersControllerTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ELearningV1._3._1.Controllers;
+using ELearningV1._3._1.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+using Microsoft.AspNetCore.Identity;
+using ELearningV1._3._1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ELearningTests
+{
+
+    [TestFixture]
+    public class UsersControllerTests
+    {
+        [Test]
+        public void Index_Called_ReturnsOk()
+        {
+            var unitOfWork = Substitute.For<IUnitOfWork>();
+            var cookieManager = Substitute.For<ICookieManager>();
+
+            var usersController = new UsersController(unitOfWork, new UserManager<User>(null, null, null, null, null, null, null, null, null), cookieManager);
+            var result = usersController.Index();
+
+        }
+
+    }
+}
